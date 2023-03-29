ec8d5380f2698cf4fec1e3a8a440ee114a8da4a6
harinder007 <harinderyadav1008@gmail.com>
2023-03-29 14:31:55 +0530

    implemented page state and options component

--- src/Components/Main/Options.js
+++ src/Components/Main/Options.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-function Options() {
+function Options(props) {
+
+  const buttons = [];
+
+  props.values.forEach(element => {
+    buttons.push(<button>{element}</button>)
+  });
+
+  console.log(buttons)
+
   return (
     <div className='options'>
-        
+        {buttons}
     </div>
   )
 }
