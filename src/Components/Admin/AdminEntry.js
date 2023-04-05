34e024343566be9819ffe6ae9f9f1abb0a50c9fd
harinder007 <harinderyadav1008@gmail.com>
2023-04-05 14:27:21 +0530

    admin entry styling

--- src/Components/Admin/AdminEntry.js
+++ src/Components/Admin/AdminEntry.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function AdminEntry({data}) {
+
+  const [year, setYear] = useState();
+  const [viewLink, setViewLink] = useState("");
+  const [downloadLink, setDownloadLink] = useState("");
+
   return (
     <div className='upload-data'>
         <div className="year-input">
         <label htmlFor="year">Year</label>
-        <input type="text" name='year'/>
+        <input type="text" name='year' onChange={(e) => setYear(e.target.value)}/>
         </div>
         <div className="link-input">
-        <label htmlFor="link">PDF Link</label>
-        <input type="text" name='year'/>
+        <label htmlFor="pdf">View Link</label>
+        <input type="text" name='pdf' onChange={(e) => setViewLink(e.target.value)}/>
+        </div>
+        <div className="link-input-2">
+        <label htmlFor="pdf1">Download Link</label>
+        <input type="text" name='pdf1' onChange={(e) => setDownloadLink(e.target.value)}/>
         </div>
         <button type='button'>Upload</button>
     </div>
