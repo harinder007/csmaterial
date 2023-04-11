725822398e9db8d3ac5db5a54f236ead69e0c4bd
harinder007 <harinderyadav1008@gmail.com>
2023-04-11 22:14:32 +0530

    implemented about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,8 +1,49 @@
 import React from 'react'
+import Navbar from './Navbar'
+import { IconButton } from '@mui/material';
+
 
 function About() {
   return (
-    <div>About</div>
+    <>
+    <Navbar/>
+    <section className="about">
+      <h1 className="about-heading">This project is developed and managed by</h1>
+      <div className="profiles">
+        <div className="profile">
+          <img src="./images/jpgs/amit.jpg" alt="profile-photo" />
+          <h2 className='name'>Amit Kharod</h2>
+          <div className="socials">
+            <IconButton><img src="./images/pngs/YouTube.png" alt="youtube-icon" /></IconButton>
+            <IconButton><img src="./images/pngs/Instagram.png" alt="instagram-icon" /></IconButton>
+            <IconButton><img src="./images/pngs/Github.png" alt="github-icon" /></IconButton>
+          </div>
+          </div>
+        <div className="profile">
+          <img src="./images/jpgs/harinder.jpg" alt="profile-photo" />
+          <h2 className='name'>Harinder Yadav</h2>
+          <div className="socials">
+            <IconButton><img src="./images/pngs/YouTube.png" alt="youtube-icon" /></IconButton>
+            <IconButton><img src="./images/pngs/Instagram.png" alt="instagram-icon" /></IconButton>
+            <IconButton><img src="./images/pngs/Github.png" alt="github-icon" /></IconButton>
+          </div>
+          </div>
+      </div>
+      <p className="about-description">"We are final year MCA students from MD University, Rohtak who are passionate about helping their fellow students. We hope you find our website helpful and informative"
+      </p>
+    </section>
+    <section id="contact">
+      <h2 class="contact-heading"><span>If you have any suggestions or feedback,</span> <span>please don't hesitate to reach out to us.</span></h2>
+      <h1 className="mail">csmaterial007@gmail.com</h1>
+    </section>
+      {/* <footer>
+        <h6 class="end-note">Made with 💙 by Amit Kharod</h6>
+        <div class="source-code">
+          <img src="./images/pngs/Github.png" alt="" />
+          <h6>Source Code</h6>
+        </div>
+      </footer> */}
+    </>
   )
 }
 
