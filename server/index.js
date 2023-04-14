94ffff2b81dfff08cde39015e5ef4ac20a24b6f8
harinder007 <harinderyadav1008@gmail.com>
2023-04-14 18:35:31 +0530

    vercel config

--- server/index.js
+++ server/index.js
@@ -23,16 +23,6 @@ app.use('/api/studyMaterial', require('./routes/api/studyMaterial'));
 app.use('/api/syllabuses', require('./routes/api/syllabuses'));
 app.use('/api/auth', require('./routes/api/auth'));
 
-// Serve static assets in production
-
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 const port = process.env.PORT || 5000;
 
